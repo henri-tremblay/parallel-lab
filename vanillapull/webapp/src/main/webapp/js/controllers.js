9b4fa32a9c112dd2b591022f290be21a07b15bdf
Henri Tremblay <henri.tremblay@gmail.com>
2013-05-16 15:03:14 +0200

    Have a working gui

--- vanillapull/webapp/src/main/webapp/js/controllers.js
+++ vanillapull/webapp/src/main/webapp/js/controllers.js
@@ -6,4 +6,18 @@ function StockCtrl($scope, $http) {
     $http.get('services/instrument/list').success(function(data) {
         $scope.stocks = data;
     });
+    $scope.price = 'Unknown';
+
+    $scope.change = function () {
+        if($scope.symbol == undefined || $scope.strike == undefined  || $scope.maturity  == undefined ) {
+            $scope.price = 'Unknown';
+            return;
+        }
+
+        var url = 'services/pricer/price?symbol=' + $scope.symbol + '&maturity=' + $scope.maturity + '&strike=' + $scope.strike;
+        $http.get(url).
+            success(function (data) {
+                $scope.price = data;
+            });
+    };
 }
